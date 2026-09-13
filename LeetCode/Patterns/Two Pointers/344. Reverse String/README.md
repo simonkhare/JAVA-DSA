c01029d46b239d05123ce427a2d4d8eb1e950db0
Simon Khare <simonkhare16@gmail.com>
2026-09-14 00:56:12 +0530

    Add Easy Java Solution for 344. Reverse String from LeetCode

--- LeetCode/Patterns/Two Pointers/344. Reverse String/README.md
+++ LeetCode/Patterns/Two Pointers/344. Reverse String/README.md
@@ -8,8 +8,8 @@
 Two Pointers, String
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 0 ms
+- **Memory:** 48.1 MB
 
 ---
 
--- LeetCode/Patterns/Two Pointers/344. Reverse String/Solution.java
+++ LeetCode/Patterns/Two Pointers/344. Reverse String/Solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
-        int l = s.length-1;
-        char c;
-        for(int i = 0 ; i <= s.length/2 ; i++){
-            c = s[i];
-            s[i] = s[l];
-            s[l] = c;
-            l--;
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
         }
     }
 }
