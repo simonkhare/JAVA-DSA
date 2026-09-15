fed7b3154a26c1318ac5957a052f89ec00365eeb
Simon Khare <simonkhare16@gmail.com>
2026-09-15 14:45:48 +0530

    Add Easy Java Solution for 9. Palindrome Number from LeetCode

---
+++ LeetCode/Java/Easy/9. Palindrome Number/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x>0){
+            int reverse = 0;
+            int temp = x ;
+            while(x>0){
+                reverse = reverse*10 + x%10 ;
+                x /= 10;
+            }
+            if(temp == reverse){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
